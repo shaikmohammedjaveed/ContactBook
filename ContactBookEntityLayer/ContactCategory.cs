01a0cea8e4e40054eabc2b8be3b609c635350498
shaikmohammedjaveed <78655258+shaikmohammedjaveed@users.noreply.github.com>
2021-02-08 13:34:40 +0530

    Add files via upload

---
+++ ContactBookEntityLayer/ContactCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBookEntityLayer
+{
+    public class ContactCategory
+    {
+        //Properties to Interact with Category table which is created in Data Base
+
+        //Property for Category Id
+        public int categoryId { get; set; }
+
+        //Property for Category Name
+        public string CategoryName { get; set; } 
+    }
+}
---
+++ ContactBookEntityLayer/Contacts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBookEntityLayer
+{
+    public class Contacts
+    {
+        // Properties to intertact with the Table which is created in Data Base
+
+
+        // Properties Category Id
+        public int CategoryId { get; set; }
+
+        // Properties Contact Id
+
+        public int ContactId { get; set; }
+
+        // Properties First Name
+        public string FirstName { get; set; }
+
+        //Properties Last Name
+        public string LastName { get; set; }
+
+        //Properties Phone Number
+        public string PhoneNo { get; set; }
+
+        //Properties Email ID
+        public string Email { get; set; }
+    }
+}
